f67a60385e54d518d4d9db38ba7ee31e10146a06
Brian <Bhensley811@gmail.com>
2022-09-07 16:39:25 -0400

    commit tree 4975528a91ec8ea6e18008b90a718b4cd331427e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+const recipient = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(recipient){
+    
+    let greetings = []
+
+    for (let i = 0; i < recipient.length; i++){
+         greetings.push(`Thank you, ${recipient[i]}, for the wonderful surprise gift!`)
+         
+    }
+    
+    return greetings
+}
+
+writeCards(recipient)
+
+
+function countDown(){
+    let countdown = 10;
+    while (countdown  >= 0) {
+      console.log(countdown--);
+    }
+
+}
